eb3e956309e7b3fed64613cdb65cd3442436cb88
mathieuchevet2 <mathieuchevet2@hotmail.fr>
2018-05-12 20:05:44 +0200

    commit 3

--- view/template.php
+++ view/template.php
@@ -5,6 +5,7 @@
 	<title>Est-ce qu'on met en prod aujourd'hui?</title>
 </head>
 <body>
+	<h1>Est-ce qu'on met en prod aujourd'hui?</h1>
 	<p>On est <?= $dayNow ?></p>
 	<p><?= $categorieMessage ?></p>
 	<p><img src="<?= $gif['urlGif'] ?>"><p>
